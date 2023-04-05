d3e4c4c665141d1f39de943118f149d5a5f91509
my1lane <123568270+my1lane@users.noreply.github.com>
2023-04-05 19:22:02 +0300

    Update ex1.cpp

--- ex1.cpp
+++ ex1.cpp
@@ -7,7 +7,7 @@ int main() {
     string some_words, reverse_some_words;
     getline(cin, some_words);
     
-    for (int n = some_words.size(); n >= 0; n--) {
+    for (int n = some_words.size() - 1; n >= 0; n--) {
         reverse_some_words.push_back(some_words[n]);
     }
     cout << reverse_some_words << endl;
